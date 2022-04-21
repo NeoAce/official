0d2bc0c0ec65cb654fd5129987e44eb777c4f5b2
Baby Jean Baido <babybaido18@gmail.com>
2022-04-21 14:21:11 +0800

    modified

--- commands/fun/ship.js
+++ commands/fun/ship.js
@@ -168,6 +168,60 @@ module.exports = {
 
                 const attachment = new MessageAttachment(canvas.toBuffer())
                 message.channel.send({content: `${message.author.username} and ${member.user.username} got ${RN}% love rate!`, files: [attachment]})
+            } else if(args[0]) {
+                let RN = Math.floor(Math.random() * 100)
+                const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+                const member1 = message.mentions.members.first(2)[1] || message.guild.members.cache.get(args[1])
+                if(!member) return message.reply("Mention a member")
+                if(!member1) return message.reply("Mention a second member")
+                if(RN > 50) {
+                    const Canvas = require('canvas')
+                    const path = require('path')
+    
+                    const canvas = Canvas.createCanvas(700, 250)
+                    const ctx = canvas.getContext('2d')
+                    const background = await Canvas.loadImage(
+                        path.join(__dirname, '../../images/background.png')
+                    )
+                    let x = 0
+                    let y = 0
+                    ctx.drawImage(background, x, y)
+    
+                    const heart = [
+                        "../../images/heart1.jpg",
+                        "../../images/heart2.jpg"
+                    ]
+                    const random = Math.floor(Math.random() * heart.length)
+                    const hearty = await Canvas.loadImage(
+                        path.join(__dirname, heart[random])
+                    )
+    
+                    x = 263
+                    y = 25
+                    ctx.drawImage(hearty, x, y, 200, 200)
+    
+                    const pfp = await Canvas.loadImage(
+                        member.user.displayAvatarURL({
+                            format: 'png',
+                        })
+                    )
+    
+                    x = 25
+                    y = 25
+                    ctx.drawImage(pfp, x, y, 200, 200)
+    
+                    const pfp1 = await Canvas.loadImage(
+                        member1.user.displayAvatarURL({
+                            format: 'png',
+                        })
+                    )
+    
+                    x = 500
+                    y = 25
+                    ctx.drawImage(pfp1, x, y, 200, 200)
+    
+                    const attachment = new MessageAttachment(canvas.toBuffer())
+                    message.channel.send({content: `${message.author.username} and ${member.user.username} got ${RN}% love rate!`, files: [attachment]})
             } else {
                 const Canvas = require('canvas')
                 const path = require('path')
@@ -218,5 +272,6 @@ module.exports = {
                 message.channel.send({content: `${message.author.username} and ${member.user.username} got ${RN}% love rate!`, files: [attachment]})
             }
         }
+    }
     }
 }
