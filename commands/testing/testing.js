a211fe38d499e43cd42d9292200b1a5dc097f504
Baby Jean Baido <babybaido18@gmail.com>
2022-04-21 14:40:42 +0800

    modified

--- commands/testing/testing.js
+++ commands/testing/testing.js
@@ -7,6 +7,23 @@ module.exports = {
     cooldown: 0,
     description: "To test something",
     async execute(client, message, args, prefix) {
-      
+        try {
+            let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]);
+            if(!role) role = message.guild.roles.cache.find(role => role.name == args[0])
+            const Members = message.guild.members.cache.filter(member => member.roles.cache.has(role.id)).map(member => member.user.tag).join('\n');
+            const embed = new MessageEmbed()
+            .setTitle(`Users with role ${role.name}:`)
+            .setDescription(`${Members}`)
+            .setColor('RANDOM')
+            .setAuthor({iconURL: `${message.author.avatarURL()}`,name: `${message.member.user.tag}`})
+            .setTimestamp()
+            message.channel.send({embeds: [embed]});
+            } catch(err) {
+              console.log(err)
+        
+              const embed1 = new MessageEmbed()
+              .setDescription(`Provide role id only or mention it.`)
+              message.channel.send({embeds: [embed1]});
+            }
     }
 }
